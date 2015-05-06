1b4e5ca00d39b633770451633108ef5a8ea9acb0
aiemsal <iemsal.assous@gmail.com>
2015-05-06 18:50:49 +0200

    commit fix
    Signed-off-by: aiemsal <iemsal.assous@gmail.com>

--- app/src/main/java/com/esilyon/fel/detailActivity.java
+++ app/src/main/java/com/esilyon/fel/detailActivity.java
@@ -1,4 +1,4 @@
-package com.supinfo.loicbillaud.fel_apps;
+package com.esilyon.fel;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class detailActivity extends ActionBarActivity {
+public class DetailActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +18,7 @@ public class detailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        getMenuInflater().inflate(R.menu.global, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
